dfaea3b61c8db99b991c6507731481193df5935f
DartMoon <storogenkokirill@gmail.com>
2016-11-19 14:20:48 +0200

    testPage

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Kirill on 12.11.2016.
+ */
+"use strict";
+
+$(document).on("ready", function () {
+		setTimeout(function () {
+			$('.change-c').css({"color":"#fd3187"})
+		}, 5000)
+		setTimeout(function () {
+			$('.change-bg').css({"background":"#fd3187"})
+		}, 5000)
+		setTimeout(function () {
+			$('.bttn__get-more').css({"background":"#fd2227"})
+		}, 5000)
+});
